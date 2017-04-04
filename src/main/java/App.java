ea11ec5ac9d7ca395a82f2fbc44ef9d70e10ed6a
ben <benjamin.mark.matthews@gmail.com>
2017-04-04 22:50:11 +0200

    Added static files and a java to json mapping

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,32 @@
 import static spark.Spark.*;
+import spark.route.RouteOverview;
+import com.google.gson.Gson;
 import net.webservicex.*;
 
 public class App {
     public static void main(String[] args) {
+
+        staticFiles.location("/public");
+
+        Gson gson = new Gson();
+
         path("/api", () -> {
-            
-            get("/hello", (req, res) -> {
+            get("/cities/:country", (request, response) -> {
                 GlobalWeather ss = new GlobalWeather();
                 GlobalWeatherSoap port = ss.getGlobalWeatherSoap12();  
-                return port.getCitiesByCountry("Norway");
+                return port.getCitiesByCountry(request.params(":country"));
             });
+
+            get("/json", (request, response) -> {
+                return new Message("A message");
+            }, gson::toJson);
         });
+            
+        notFound((request, response) -> {
+            response.redirect("/notfound.html");
+            return "";
+        });
+
+        RouteOverview.enableRouteOverview("/api");      
     }
 }
